73f16ce11b9e4cf07354b4fb97c5021d169c9ed7
vivekraju6495 <vivekraju1995@outlook.com>
2026-02-16 18:13:21 -0500

    fix

--- backend/app/api/v1/routes/projects.py
+++ backend/app/api/v1/routes/projects.py
@@ -7,17 +7,20 @@ from app.api.v1.controllers.project_controller import (
     get_all_projects_controller,
     get_project_by_uuid_controller
 )
+from app.core.response import success_response, error_response
+from app.schemas.common import ResponseModel
 
 router = APIRouter()
 
 @router.get("/", response_model=list[ProjectSchema])
 def get_all_projects(db: Session = Depends(get_db)):
-    return get_all_projects_controller(db)
-
+    data = get_all_projects_controller(db)
+    return success_response("Projects fetched successfully", data)
 
 @router.get("/{uuid}", response_model=ProjectSchema)
 def get_project_by_uuid(uuid: str, db: Session = Depends(get_db)):
     try:
-        return get_project_by_uuid_controller(db, uuid)
+        data = get_project_by_uuid_controller(db, uuid)
+        return success_response("Projects fetched successfully", data)
     except Exception as e:
-        raise HTTPException(status_code=404, detail=str(e))
+        return error_response(str(e), status=404)
